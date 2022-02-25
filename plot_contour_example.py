a49fd8011422ebb0148affc90592ffd8f1bba0bd
Jamie <69422343+jsm8989@users.noreply.github.com>
2022-02-25 15:35:28 +0000

    Update plot_contour_example.py

--- plot_contour_example.py
+++ plot_contour_example.py
@@ -1,24 +1,25 @@
 import sys
-sys.path.append("python-temika-reader")
-sys.path.append("contour_analyzer")
-#This is a simple script to open the movie, load the full contour, and plot the contour overlayed on the image.
-
-from read_cpp_contour import load_contour_cpp
+sys.path.append("../python-temika-reader")
+sys.path.append("../contour_analyzer")
+from contour_fitter import load_contour_cpp
 from pytmk import Movie
 import matplotlib.pyplot as plt
 
-m = Movie("well_C5_cell_0000.movie") #Loads the movie
-c = load_contour_cpp('well_C5_cell_0000_contour_full.txt') #Loads the full contour
+m = Movie("../well_C5_cell_0000.movie") 
+c = load_contour_cpp('../well_C5_cell_0000_contour_full.txt') # pre-produced by C++, don't currently have for any other files
 
-#Define a function to plot the full contour over the image
+
 def plot_contour(i):
+	"""
+	Plot entire contour over image
+	"""
 	plt.imshow(m.get_frame(i))
 	if c[i][1] is not None:
 		plt.plot(*c[i][1].T, c = 'r')
 
 
 
-#As an example, plot the 10th frame and contour together
+# eg 10th frame and contour 
 plot_contour(10)
-
 plt.show()
+
