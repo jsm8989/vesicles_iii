f78eaf00ae2724317f153f3b152f44f343ce0888
Jamie Morley <jsm89@BSS.PHY.PRIVATE.CAM.AC.UK>
2022-05-13 17:08:57 +0100

    updates

--- scripts/plotsecondshutter.py
+++ scripts/plotsecondshutter.py
@@ -9,19 +9,16 @@ import matplotlib.pyplot as plt
 
 #filename= 'BF_2.28Mar2022_15.42.44_8.20_forPython.mat'
 
-fig, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=True)
+#fig, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=True)
 for file in os.listdir('Z:\code\jamie_matlab_scripts\\'):
-    if "v1_BF_20x_shutter" in str(file):
+    if "v3_BF_20x_shutter" in str(file):
         #print(file)
         data1 = sio.loadmat('Z:\code\jamie_matlab_scripts\\'+file)
 
         #print(data)
-        time = float(file[18:26])
-        if time<0.001:
-            #print(time)
-            ax1.plot(data1['q'][4:20], data1['ps'][4:20]*(10**18), label=str(file[18:26])+"s") # q_min: q_max
-        else:
-            ax2.plot(data1['q'][4:20], data1['ps'][4:20]*(10**18), label=str(file[18:26])+"s") # q_min: q_max
+        #time = float(file[18:26])
+        plt.plot(data1['q'][4:20], data1['ps'][4:20]*(10**18), label=str(file[18:26])+"s") # q_min: q_max
+        
         
         
         #plt.plot(data1['q'][4:20], data1['ps'][4:20], label=str(file[18:26])+"s") # q_min: q_max
@@ -29,9 +26,7 @@ for file in os.listdir('Z:\code\jamie_matlab_scripts\\'):
 
 plt.suptitle("Raw PS for different shutter times for vesicle of radius 12um")#+filename[:-14])
 plt.xlabel("q (1/m)")
-#plt.ylabel("mean square amplitude (m^2)")
-ax1.set(ylabel=r'$\langle |u(q)|^2 \rangle$ (nm$^2$)')
-ax2.set(ylabel=r'$\langle |u(q)|^2 \rangle$ (nm$^2$)')
-ax1.legend()
-ax2.legend()
+plt.ylabel(r'$\langle |u(q)|^2 \rangle$ (nm$^2$)')
+plt.legend()
+
 plt.show()
